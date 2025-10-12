0cb1084540eab29a1a376ef6984b70d7984032bb
reagan luyinda <luyindareagan@gmail.com>
2025-10-12 15:00:06 +0300

    Ref; Removed redudancy from the content component in the App file

--- course-info/src/App.jsx
+++ course-info/src/App.jsx
@@ -11,40 +11,17 @@ const App = () => {
   const part3 = "State of a component";
   const exercises3 = 14;
 
-  let x = "reagen";
-  const y = [1, -1, 3];
-  // console.log(x);
-
-  y.push(5);
-  // console.log(y);
-  // console.log(y.length);
-  // y.forEach((value) => {
-  //   console.log(value);
-  // });
-  // const y2 = y.concat(4);
-  // console.log(y2);
-  // console.log(y);
-
-  const m1 = y.map((value) => value * 2);
-  console.log(m1);
-
   return (
     <>
-      {/* <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> */}
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} />
-      <Content part2={part2} exercises2={exercises2} />
-      <Content part3={part3} exercises3={exercises3} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
       <Total total={exercises1 + exercises2 + exercises3} />
     </>
   );
