f4bd68df3673fb471964bc86f7be2debcf361379
reagan luyinda <luyindareagan@gmail.com>
2025-10-12 15:34:10 +0300

    Course info 1.3 step 3

--- course-info/src/App.jsx
+++ course-info/src/App.jsx
@@ -4,26 +4,26 @@ import Total from "./components/Total";
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = { name: "Fundamentals of React", exercises1: 10 };
+
+  const part2 = { name: "Using props to pass data", exercises2: 7 };
+
+  const part3 = { name: "State of a component", exercises3: 14 };
 
   return (
-    <>
-      <Header course={course} />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
-      <Total total={exercises1 + exercises2 + exercises3} />
-    </>
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1.name} {part1.exercises1}
+      </p>
+      <p>
+        {part2.name} {part2.exercises2}
+      </p>
+      <p>
+        {part3.name} {part3.exercises3}
+      </p>
+      <p>Total = {part1.exercises1 + part2.exercises2 + part3.exercises3} </p>
+    </div>
   );
 };
 
