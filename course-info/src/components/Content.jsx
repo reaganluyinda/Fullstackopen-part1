dbaa3abf074f3f5b7191b9c65ccbf7ce3566f5ae
reagan luyinda <luyindareagan@gmail.com>
2025-10-13 12:03:32 +0300

    Completed Course info 1.4 step 4

--- course-info/src/components/Content.jsx
+++ course-info/src/components/Content.jsx
@@ -1,10 +1,11 @@
 import Part from "./Part.jsx";
 const Content = (props) => {
+  console.log(props);
   return (
     <div>
-      <Part name={props.part1} exercises={props.exercises1} />
-      <Part name={props.part2} exercises={props.exercises2} />
-      <Part name={props.part3} exercises={props.exercises3} />
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
     </div>
   );
 };
--- course-info/src/components/Total.jsx
+++ course-info/src/components/Total.jsx
@@ -1,7 +1,13 @@
 const Total = (props) => {
+  console.log(props);
   return (
     <div>
-      <p>Number of exercises {props.total}</p>
+      <p>
+        Number of exercises{" "}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
+      </p>
     </div>
   );
 };
